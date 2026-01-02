6bec2c6290de0d0feaa120d64e90917bd3b4c989
Mayur Borse <mayur.borse@ilink-systems.com>
2026-01-02 09:52:19 +0530

    Added Schema.pre, logical queries, Handled corner cases for connectionRequest API

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -19,6 +19,17 @@ const connectionRequestSchema = new mongoose.Schema({
     }
 }, {
     timestamps: true,
+});
+
+// Compound indexing
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1});
+
+connectionRequestSchema.pre('save', function (next) {
+    const connectionRequest = this;
+    // Check fromUserId == toUserId
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId))
+        throw new Error("Cannot send connection request to yourself!");
+    next();
 })
 
 module.exports = mongoose.model("ConnectionRequest",connectionRequestSchema);
--- src/routes/requests.js
+++ src/routes/requests.js
@@ -2,6 +2,7 @@ const express = require('express');
 const requestRouter = express.Router();
 const {userAuth} = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
 requestRouter.post("/sendConnectionRequest", userAuth, async (req,res) => {
     console.log("Sending a connection request!");
@@ -17,6 +18,25 @@ requestRouter.post(
             const fromUserId = req.user._id;
             const toUserId = req.params.toUserId;
             const status = req.params.status;
+
+            const allowedStatus = ["ignored","interested"];
+            if(!allowedStatus.includes(status))
+                return res.status(400).json({message: "Invalid Status Type: "+ status});
+
+            const toUser = await User.findById(toUserId);
+            if(!toUser)
+                return res.status(400).json({message: "User not found!"});
+
+            const existingConnectionRequest = await ConnectionRequest.findOne({
+                status: status,
+                $or: [
+                    { fromUserId, toUserId },
+                    { fromUserId: toUserId, toUserId: fromUserId }
+                ]
+            });
+
+            if(existingConnectionRequest)
+                return res.status(200).json(({message: "Connection request already exists!"}));
 
             const connectionRequest = ConnectionRequest({
                 fromUserId,
