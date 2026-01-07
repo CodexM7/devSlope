4036ba115ac73a34dc0bfa19d1a67f9b3cbbce1e
Mayur Borse <mayur.borse@ilink-systems.com>
2026-01-07 20:37:05 +0530

    Added pagination for feed endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -63,17 +63,26 @@ userRouter.get("/user/connections", userAuth, async (req,res) => {
 userRouter.get("/feed", userAuth, async (req,res) => {
     try {
         const loggedInUser = req.user;
+        const page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limt > 50 ? 50 : limit;
+        const skip = (page-1) * limit;
+
         const connectionRequests = await connectionRequest.find({
             $or: [{ fromUserId: loggedInUser._id}, {toUserId: loggedInUser._id}]
         }).select("fromUserId toUserId");
+
         const hideUsersFromFeed = new Set();
         connectionRequests.forEach(req => {
             hideUsersFromFeed.add(req.fromUserId.toString());
             hideUsersFromFeed.add(req.toUserId.toString());
         })
+
         const users = await user.find({
             _id: {$nin: Array.from(hideUsersFromFeed)}
-        }).select(["firstName", "lastName", "photoUrl", "gender", "age", "about", "skills"]);
+        }).select(["firstName", "lastName", "photoUrl", "gender", "age", "about", "skills"])
+        .skip(skip).limit(limit);
+
         res.status(200).json(users);
     } catch (err) {
         res.status(400).send("Error: "+err.message);
